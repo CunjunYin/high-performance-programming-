b90a18c4928299056b6c03afe3eb43e8933dda64
CunjunYin <42023214+CunjunYin@users.noreply.github.com>
2018-08-23 13:05:50 +0800

    Add files via upload
    update rule detection

--- Programming Assignment 1/GoL.c
+++ Programming Assignment 1/GoL.c
@@ -11,9 +11,15 @@
 #include "linkedList.h"
 
 // compile with gcc-6 -fopenmp -o lab1 lab1.c
+#define ALIVE 1
+#define DEAD -1
 
 //header
 int**malloc2DArray(int, int)
+
+//Globale Controlling Variable
+int MAXROW = 0, MAXCOL = 0;
+int **gameArr;
 
 // functions
 int main(int argc, char *argv[]){
@@ -21,32 +27,91 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
     else {
+        MAXROW = MAXCOL = my_atoi(argv[1]);
         exit(EXIT_SUCCESS);
     }
     return 0;
 }
 
-int**malloc2DArray(int m, int n)
-{
+int**malloc2DArray(int m, int n){
     int**array = (int**)malloc(m*sizeof(int*));
     
-    if(array == NULL)
-    {
+    if(array == NULL){
         fprintf(stderr,"out of memory");
         exit(EXIT_FAILURE);
     }
-#pragma omp parallel
-    for(int i = 0; i < m; i++)
-    {
+//#pragma omp parallel
+    for(int i = 0; i < m; i++){
         array[i] = (int*)malloc(n*sizeof(int));
-        if(array[i]==NULL)
-        {
+        if(array[i]==NULL){
             fprintf(stderr,"out of memory");
             exit(EXIT_FAILURE);
         }
     }
     return array;
 }
+
+int my_atoi(const char *str){
+    int num = 0;
+    if (*str == '-') str++;
+    else if (*str == '+') str++;
+    
+    while (*str != '\0'){
+        if ((*str < '0') || (*str > '9')) exit(EXIT_FAILURE);
+        num = num * 10 + (*str - '0');
+        str++;
+    }
+    
+    return num;
+}
+
+int detect(int row, int col){
+    int count = 0;
+    int r = row - 1; if( r < 0 ) r = 0;
+    int c = col - 1; if( c < 0 ) c = 0;
+    int maxR = row + 1; if( maxR > MAXROW) maxR = MAXROW;
+    int macC = col + 1; if( macR > MAXCOL) macR = MAXCOL;
+    
+    for(r = row - 1; r <= row+1; r++){
+        for(c = col - 1; c <= col+1; c++){
+            if(gameArr[r][c] == ALIVE) count++;
+        }
+    }
+
+    if(gameArr[row][col] == ALIVE) count--;
+    return count;
+}
+
+int init(void){
+    
+}
+
+void fate(int col, int row){
+    switch (detect(row, col)) {
+        case 0:
+            statements
+            break;
+        case 1:
+            <#statements#>
+            break;
+        case 2:
+            <#statements#>
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
