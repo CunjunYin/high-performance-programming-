c3ff657e634e37245010abac7cf1f109c5e41f75
CunjunYin <42023214+CunjunYin@users.noreply.github.com>
2018-10-26 11:34:52 +0800

    Add files via upload

--- programming asses 3/linkedList.c
+++ programming asses 3/linkedList.c
@@ -8,25 +8,39 @@
 #include <stdlib.h>
 #include "linkedList.h"
 
-void display(node* head){
-    node *display = head;
-    while (display->next!=NULL) {
-        printf("%d %d %f\n",display->row,display->col,display->data);
-        display = display->next;
+void display(queue *head){
+    if(head == NULL){
+        printf("NULL\n");
+    }
+    else{
+        printf("%f\n", head -> data);
+        display(head->next);
     }
 }
 
-void push(node * head, int row, int col,float data){
-    // create a new node, to append at the start
-    node * new_node;
-    new_node = malloc(sizeof(node));
-    
-    // link rechange
-    new_node->row = row;
-    new_node->col = col;
-    new_node->data = data;
-    new_node->next = head;
-    *head = *new_node;
+int isempty(queue *q){
+    return (q->next == NULL);
 }
 
+
+void push(queue* node, int row, int col, float data){
+    queue* temp = malloc(sizeof(queue));
+    temp->row = row;
+    temp->col = col;
+    temp->data = data;
+}
+
+void i_toString(int* a, int start, int end){
+    for(int i= start; i <= end; i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+
+void f_toString(float* a, int start, int end){
+    for(int i= start; i <= end; i++){
+        printf("%f ",a[i]);
+    }
+    printf("\n");
+}
 
--- programming asses 3/linkedList.h
+++ programming asses 3/linkedList.h
@@ -7,15 +7,15 @@
 
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+#endif
 typedef struct matrix {
     int row;
     int col;
     float data;
     struct matrix * next;
-} node;
+} queue;
 
-void display(node*);
-void push(node* , int , int , float);
-#endif
-
+void display(queue*);
+void push(queue* , int , int , float);
+void i_toString(int* , int, int);
+void f_toString(float* , int, int);
