a9581551c2bcd838928ee43dd202a3a702276baa
Qianhao Liu <32607463+Qianhao96@users.noreply.github.com>
2018-11-02 11:14:49 +0800

    Update MPI_Final_send.c

--- programming asses 3/MPI_Final_send.c
+++ programming asses 3/MPI_Final_send.c
@@ -141,10 +141,10 @@ float perofrmCaculation(int* f1_col, float* f1_data, int* f2_row, float* f2_data
 
 float caculation(int rowC, int colC,
                  int* f1_row, int* f1_col, float* f1_data, int f1_size,
-                 int* f2_row, int* f2_col, float* f2_data, int f2_size, int index1, int index2){
+                 int* f2_row, int* f2_col, float* f2_data, int f2_size,
+                int index1, int index2){
     float result = 0;
     int startIndex1 = index1, endIndex1 = index2, startIndex2 = -1, endIndex2 = -1;
-    
     bool found = false;
     for(int i = 0; i < f2_size; i++){
         if(f2_col[i] == colC && found == false){
@@ -154,6 +154,8 @@ float caculation(int rowC, int colC,
         if(found == true){
             if(f2_col[i] == colC){
                 endIndex2 = i;
+            }else{
+                break;
             }
         }
     }
@@ -172,28 +174,32 @@ void matrixMutilplication(int* f1_row, int* f1_col, float* f1_data,
                           int* f2_row, int* f2_col, float* f2_data,
                           int sizeOne, int sizeTwo){
     int j;
+    int startIndex1 = -1,endIndex1 = -1;
+    bool found;
 #pragma omp parallel reduction(+:result_size)
     {
         //TODO change the sizeOfMatrix to the send size;
         for(int i = f1_row[0]; i <= f1_row[sizeOne-1]; i++ ){
-            int startIndex1 = -1,endIndex1 = -1
-            bool found = false;
-            for(int i = 0; i < f1_size; i++){
-                if(f1_row[i] == rowC && found == false){
-                    startIndex1 = i;
+            startIndex1 = -1;
+            found = false;
+            for(int k = endIndex1+1; k < sizeOne; k++){
+                if(f1_row[k] == i && found == false){
+                    startIndex1 = k;
                     found = true;
                 }
                 if(found == true){
-                    if(f1_row[i] == rowC){
-                        endIndex1 = i;
+                    if(f1_row[k] == i){
+                        endIndex1 = k;
+                    }else{
+                        break;
                     }
                 }
             }
             if(found == false) continue;
-            if(found == true && endIndex1 == -1) endIndex1 = f1_size-1;
+            if(found == true && endIndex1 == -1) endIndex1 = sizeOne-1;
             
             #pragma omp for firstprivate(i) private(j)
-            for(int j =1; j <= sizeOfMatrix; j++ ){
+            for(j =1; j <= sizeOfMatrix; j++ ){
                 float result = caculation(i, j, f1_row, f1_col, f1_data, sizeOne, f2_row, f2_col, f2_data, sizeTwo, startIndex1, endIndex1);
                 if(result != 0) {
                     //printf("%d %d %f\n",i,j,result);
@@ -203,6 +209,7 @@ void matrixMutilplication(int* f1_row, int* f1_col, float* f1_data,
             }
         }
     }
+    printf("%d\n",result_size);
 }
 
 void init(){
